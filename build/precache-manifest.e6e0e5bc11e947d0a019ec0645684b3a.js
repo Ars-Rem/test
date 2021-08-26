653b6332f49444b7c78d2e6e4ecc53e88a83b12b
Ars-Rem <1800gsm@gmail.com>
2021-08-26 18:19:10 +0300

    adds

---
+++ build/precache-manifest.e6e0e5bc11e947d0a019ec0645684b3a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab00444e2412720515e497263a6ba136",
+    "url": "/index.html"
+  },
+  {
+    "revision": "40994afbf113837a9554",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "fa19f0a574014897710d",
+    "url": "/static/js/2.b6097d3d.chunk.js"
+  },
+  {
+    "revision": "40994afbf113837a9554",
+    "url": "/static/js/main.7af6c6ec.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
